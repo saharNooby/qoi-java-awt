dc903275303ebc027321debfdfd2caf26f88ee48
saharNooby <saharnooby@protonmail.com>
2021-12-25 16:05:31 +0500

    Rewrite benchmark

--- src/test/java/me/saharnooby/qoi/benchmark/FormatComparisonBenchmark.java
+++ src/test/java/me/saharnooby/qoi/benchmark/FormatComparisonBenchmark.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.IndexColorModel;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -28,7 +29,7 @@ public final class FormatComparisonBenchmark {
 
 	private enum Format {
 
-		QOI, PNG, JPG, BMP
+		QOI, PNG
 
 	}
 
@@ -40,75 +41,241 @@ public final class FormatComparisonBenchmark {
 		}
 
 		// Settings
-		int warmUpSec = 5;
-		int runSec = 5;
+		int warmUpSeconds = 5;
+		int runSeconds = 5;
+		int maxImages = 50;
 
 		// ---
 
 		// Disable cache so file IO does not skew the results
 		ImageIO.setUseCache(false);
 
+		List<File> files = findFiles(dir);
+
+		if (files.size() > maxImages) {
+			files.sort(Comparator.naturalOrder());
+
+			Collections.shuffle(files, new Random(1));
+
+			System.out.println("Using " + maxImages + " images out of " + files.size());
+
+			files = files.subList(0, maxImages);
+		}
+
 		List<Image> images = new ArrayList<>();
 
-		for (File file : Objects.requireNonNull(dir.listFiles())) {
+		for (File file : files) {
 			images.add(new Image(file));
 		}
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream(16 * 1024 * 1024);
 
+		int total = images.size() * Mode.values().length * Format.values().length;
+
+		int done = 0;
+
 		for (Image image : images) {
-			System.out.println(image.file.getName() + ", " + image.image.getWidth() + " x " + image.image.getHeight());
-
-			for (Mode mode : Mode.values()) {
-				System.out.println("\t" + mode.name().toLowerCase());
-
+			for (boolean encode : new boolean[] {false, true}) {
 				for (Format format : Format.values()) {
-					if (!image.isFormatSupported(format)) {
-						continue;
-					}
+					// Display progress
+					System.out.println("[" + ++done + "/" + total + "] " + image.file.getName() + ", " + encode + ", " + format);
+
+					Result result = image.results.computeIfAbsent(format, k -> new Result());
 
 					int encodedSize = 0;
 
-					List<Double> delays = new ArrayList<>();
+					List<Double> iterationDelays = new ArrayList<>();
 
 					long start = System.nanoTime();
 
 					while (true) {
 						out.reset();
 
-						long nano = System.nanoTime();
+						long iterationStart = System.nanoTime();
 
-						if (mode == Mode.DECODE) {
-							blackHole = Objects.requireNonNull(ImageIO.read(new ByteArrayInputStream(image.encoded.get(format))));
-						} else {
+						if (encode) {
 							if (!ImageIO.write(image.image, format.name(), out)) {
 								throw new IllegalStateException("Failed to write " + format);
 							}
 
 							blackHole = encodedSize = out.size();
+						} else {
+							blackHole = Objects.requireNonNull(ImageIO.read(new ByteArrayInputStream(image.encoded.get(format))));
 						}
 
-						double delay = (System.nanoTime() - nano) / 1_000_000D;
+						long iterationDelay = System.nanoTime() - iterationStart;
 
 						long totalDelay = System.nanoTime() - start;
 
-						if (totalDelay > TimeUnit.SECONDS.toNanos(warmUpSec + runSec)) {
+						if (totalDelay > TimeUnit.SECONDS.toNanos(warmUpSeconds + runSeconds)) {
 							break;
 						}
 
-						if (totalDelay > TimeUnit.SECONDS.toNanos(warmUpSec)) {
-							delays.add(delay);
+						if (totalDelay > TimeUnit.SECONDS.toNanos(warmUpSeconds)) {
+							iterationDelays.add(iterationDelay / 1_000_000D);
 						}
 					}
 
-					double ms = delays.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+					double averageDelay = iterationDelays.stream().mapToDouble(Double::doubleValue).average().orElse(0);
 
-					String timeFormatted = String.format(Locale.ROOT, "%.3f ms", ms);
-
-					System.out.println("\t\t" + format + ": " + timeFormatted + (encodedSize > 0 ? ", " + encodedSize / 1024 + " KB" : ""));
+					if (encode) {
+						result.encode = averageDelay;
+						result.size = encodedSize;
+					} else {
+						result.decode = averageDelay;
+					}
 				}
 			}
 		}
+
+		// Print results
+		System.out.println();
+		System.out.println(System.getProperty("java.runtime.name") + " " + System.getProperty("java.runtime.version"));
+		System.out.println("Warm up: " + warmUpSeconds + " seconds, run: " + runSeconds + " seconds");
+		System.out.println(images.size() + " files total");
+
+		System.out.println();
+		System.out.println("Average over all files");
+		printTable(average(images));
+
+		for (Image image : images) {
+			System.out.println();
+			System.out.println(image.file.getName() + ", " + image.image.getWidth() + " x " + image.image.getHeight());
+
+			printTable(image.results);
+		}
+	}
+
+	private static Map<Format, Result> average(@NonNull Collection<Image> images) {
+		if (images.isEmpty()) {
+			return Collections.emptyMap();
+		}
+
+		Set<Format> keys = EnumSet.noneOf(Format.class);
+
+		for (Image image : images) {
+			if (keys.isEmpty()) {
+				keys.addAll(image.results.keySet());
+			} else {
+				keys.retainAll(image.results.keySet());
+			}
+		}
+
+		Map<Format, Result> average = new EnumMap<>(Format.class);
+
+		for (Format key : keys) {
+			average.put(key, new Result());
+		}
+
+		for (Image image : images) {
+			for (Format key : keys) {
+				Result imageRes = image.results.get(key);
+
+				Result sum = average.get(key);
+
+				sum.decode += imageRes.decode;
+				sum.encode += imageRes.encode;
+				sum.size += imageRes.size;
+			}
+		}
+
+		for (Result result : average.values()) {
+			result.decode /= images.size();
+			result.encode /= images.size();
+			result.size /= images.size();
+		}
+
+		return average;
+	}
+
+	private static void printTable(@NonNull Map<Format, Result> results) {
+		if (results.isEmpty()) {
+			System.out.println("No results");
+
+			return;
+		}
+
+		int width = 14;
+
+		System.out.println(pad("Format", 0, 8) +
+				pad("Decode", width * 2, 0) +
+				pad("Encode", width * 2, 0) +
+				pad("Size", width * 2, 0));
+
+		for (Map.Entry<Format, Result> e : results.entrySet()) {
+			Result result = e.getValue();
+
+			Result qoi = results.get(Format.QOI);
+
+			System.out.println(pad(e.getKey().name(), 0, 8) +
+					pad(formatMs(result.decode), width, 0) +
+					pad(compare(result.decode, qoi.decode), width, 0) +
+					pad(formatMs(result.encode), width, 0) +
+					pad(compare(result.encode, qoi.encode), width, 0) +
+					pad(result.size / 1024 + " KB", width, 0) +
+					pad(compare(result.size, qoi.size), width, 0));
+		}
+	}
+
+	private static String compare(double value, double comparedTo) {
+		if (value == comparedTo) {
+			return "";
+		}
+
+		double percent = (value - comparedTo) / comparedTo * 100;
+
+		return (percent > 0 ? "+" : "") + (int) percent + "%";
+	}
+
+	private static String formatMs(double v) {
+		return String.format(Locale.ROOT, "%.3f ms", v);
+	}
+
+	@SuppressWarnings("StringConcatenationInLoop")
+	private static String pad(@NonNull String s, int left, int right) {
+		while (s.length() < left) {
+			s = " " + s;
+		}
+
+		while (s.length() < right) {
+			s = s + " ";
+		}
+
+		return s;
+	}
+
+	private static List<File> findFiles(@NonNull File root) {
+		List<File> result = new ArrayList<>();
+
+		LinkedList<File> stack = new LinkedList<>();
+
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+			File dir = stack.pop();
+
+			for (File file : Objects.requireNonNull(dir.listFiles())) {
+				if (file.isDirectory()) {
+					stack.push(file);
+				} else {
+					if (file.getName().endsWith(".txt")) {
+						continue;
+					}
+
+					result.add(file);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private static final class Result {
+
+		double decode;
+		double encode;
+		long size;
+
 	}
 
 	private static final class Image {
@@ -116,30 +283,39 @@ public final class FormatComparisonBenchmark {
 		final File file;
 		final BufferedImage image;
 		final Map<Format, byte[]> encoded = new EnumMap<>(Format.class);
+		final Map<Format, Result> results = new EnumMap<>(Format.class);
 
 		public Image(@NonNull File file) throws Exception {
 			this.file = file;
-			this.image = ImageIO.read(file);
+
+			BufferedImage image = Objects.requireNonNull(ImageIO.read(file), "Invalid image in " + file);
+
+			if (image.getColorModel() instanceof IndexColorModel) {
+				// QOI can't write indexed images directly, we need to convert it to normal type
+				int type = image.getTransparency() == Transparency.OPAQUE ?
+						BufferedImage.TYPE_3BYTE_BGR :
+						BufferedImage.TYPE_4BYTE_ABGR;
+
+				BufferedImage copy = new BufferedImage(image.getWidth(), image.getHeight(), type);
+				Graphics g = copy.getGraphics();
+				g.drawImage(image, 0, 0, null);
+				g.dispose();
+				image = copy;
+			}
+
+			this.image = image;
 
 			ByteArrayOutputStream out = new ByteArrayOutputStream(16 * 1024 * 1024);
 
 			for (Format format : Format.values()) {
-				if (isFormatSupported(format)) {
-					out.reset();
+				out.reset();
 
-					ImageIO.write(this.image, format.name(), out);
-
-					this.encoded.put(format, out.toByteArray());
+				if (!ImageIO.write(this.image, format.name(), out)) {
+					throw new IllegalStateException("Failed to encode " + file + " as " + format);
 				}
-			}
-		}
 
-		public boolean isFormatSupported(@NonNull Format format) {
-			if (format == Format.JPG || format == Format.BMP) {
-				return this.image.getTransparency() == Transparency.OPAQUE;
+				this.encoded.put(format, out.toByteArray());
 			}
-
-			return true;
 		}
 
 	}
